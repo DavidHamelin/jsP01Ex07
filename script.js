db64c0cc200e1f340dad61c6d9ea6e5937eef42b
DavidHamelin <hamelindavid@hotmail.fr>
2019-01-11 10:10:16 +0100

    js ex 07 regexNum included

---
+++ script.js
@@ -0,0 +1,22 @@
+﻿function calculator()
+{
+    var shoeSize = document.getElementById('shoeSize').value;
+    var yearOfBirth = document.getElementById('yearOfBirth').value;
+    var result = ((((shoeSize *= 2) + 5) * 50) - yearOfBirth) + 1769;
+    /* identique à ce calcul :
+    shoeSize *= 2
+    shoeSize + 5;
+    shoeSize * 50;
+    var result = (shoeSize - yearOfBirth) + 1769;*/
+
+    var regexNum = /^[0-9.,]+$/i; /* n'accepte que les nombres */
+    if (regexNum.test(shoeSize) == false) {
+        alert('Pointure non valide')
+    }
+    else if (regexNum.test(yearOfBirth) == false) {
+        alert('Année de naissance non valide')
+    }
+    else {
+        alert(`La calculette a trouvé (après un calcul tordu) : ${result}`);
+    }
+}
