a59728cc625cbbd7bf8764bbd8e392e2a8d3501c
DavidHamelin <hamelindavid@hotmail.fr>
2019-01-11 10:48:14 +0100

    Ex 07 all regex ok included

--- script.js
+++ script.js
@@ -2,20 +2,11 @@
 {
     var shoeSize = document.getElementById('shoeSize').value;
     var yearOfBirth = document.getElementById('yearOfBirth').value;
-    var result = ((((shoeSize *= 2) + 5) * 50) - yearOfBirth) + 1769;
-    /* identique à ce calcul :
-    shoeSize *= 2
-    shoeSize + 5;
-    shoeSize * 50;
-    var result = (shoeSize - yearOfBirth) + 1769;*/
 
     var regexNum = /^[0-9.,]+$/i; /* n'accepte que les nombres */
-    if (regexNum.test(shoeSize) == false) {
-        alert('Pointure non valide')
-    }
-    else if (shoeSize > 60)
+    if (regexNum.test(shoeSize) == false)
     {
-        alert('Pointure trop grande')
+        alert('Pointure non valide')
     }
     else if (regexNum.test(yearOfBirth) == false)
     {
@@ -25,7 +16,18 @@
     {
         alert('Année de naissance trop lointaine')
     }
-    else {
+    else if (shoeSize > 60)
+    {
+        alert('Pointure trop grande')
+    }
+    else
+    {
+        var result = ((((shoeSize *= 2) + 5) * 50) - yearOfBirth) + 1769;
+        /* identique à ce calcul :
+        shoeSize *= 2
+        shoeSize + 5;
+        shoeSize * 50;
+        var result = (shoeSize - yearOfBirth) + 1769;*/
         alert(`La calculette a trouvé (après un calcul tordu) : ${result}`);
     }
 }
